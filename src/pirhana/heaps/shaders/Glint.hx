1bf717d4719653adb0656f29b493d55cef20bd77
Christophe Bessette <christophe.bessette@hotmail.com>
2024-03-12 13:35:15 -0400

    fixed glint shader using full textures on subtiles

--- src/pirhana/heaps/shaders/Glint.hx
+++ src/pirhana/heaps/shaders/Glint.hx
@@ -1,29 +1,28 @@
 package pirhana.heaps.shaders;
 
-class Glint extends hxsl.Shader {
+class Glint extends hxsl.Shader
+{
+	static var SRC =
+		{
+			@:import h3d.shader.Base2d;
+			@param var speed:Float = 0.6;
+			@param var linewidth:Float = 0.8;
+			@param var grad:Float = 3.0;
+			@param var intensity:Float = 0.6;
+			function fragment()
+			{
+				var col1 = vec4(0.3, 0.0, 0.0, 1.0);
+				var col2 = vec4(0.85, 0.85, 0.85, 1.0);
+				var color = textureColor;
+				var linepos = input.uv;
 
-    static var SRC = {
-        @:import h3d.shader.Base2d;
-        
-        @param var speed : Float = 0.6;
-        @param var linewidth : Float = 0.8;
-        @param var grad : Float = 3.0;
-        @param var intensity : Float = 0.6;
-        
-        function fragment() {
-            var col1 = vec4(0.3, 0.0, 0.0, 1.0);
-            var col2 = vec4(0.85, 0.85, 0.85, 1.0);
-            var color = textureColor;
-            var linepos = calculatedUV;
+				linepos.x = linepos.x - mod(time * speed, 2.0) + 0.5;
 
-            linepos.x = linepos.x - mod(time*speed,2.0)+0.5;
+				var y = linepos.x * grad;
+				var s = smoothstep(y - linewidth, y, linepos.y) - smoothstep(y, y + linewidth, linepos.y);
 
-            var y = linepos.x * grad;
-            var s = smoothstep(y-linewidth, y, linepos.y) - smoothstep(y, y+linewidth, linepos.y);
-
-            pixelColor = color + ((s*col1)+(s*col2)) * intensity;
-            pixelColor.a = color.a;
-        }
-    }
-
+				pixelColor = color + ((s * col1) + (s * col2)) * intensity;
+				pixelColor.a = color.a;
+			}
+		}
 }
