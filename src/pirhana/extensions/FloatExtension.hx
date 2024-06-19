165c3fbd5c6ea06f9639564d84f38fd745cad9ed
Christophe Bessette <christophe.bessette@hotmail.com>
2024-06-19 09:57:40 -0400

    added wrap to float extension

--- src/pirhana/extensions/FloatExtension.hx
+++ src/pirhana/extensions/FloatExtension.hx
@@ -27,4 +27,20 @@ class FloatExtension
 	public static function under(me:Float, max:Float){
 		return me > max ? max : me;
 	}
+
+	/**
+		Wraps a number around min and max, inclusive.
+
+		e.g.: 6.wrap(0, 5) = 0;
+
+		e.g.: -1.wrap(0, 5) = 5;
+	**/
+	public static function wrap(me:Float, min:Float, max:Float)
+		{
+			// https://stackoverflow.com/questions/707370/clean-efficient-algorithm-for-wrapping-integers-in-c
+			var range = max - min + 1;
+			if (me < min)
+				return me + Std.int(range * ((min - me) / range + 1)) - 1;
+			return min + (me - min) % range;
+		}
 }
