a96774ffd65d5c78d5d4398f8bcdd2dea92869e8
Christophe Bessette <christophe.bessette@hotmail.com>
2024-09-25 15:58:43 -0400

    fixed state stuff

--- src/pirhana/utils/state/IState.hx
+++ src/pirhana/utils/state/IState.hx
@@ -1,15 +1,15 @@
 package pirhana.utils.state;
 
-interface IState{
+interface IState {
+	/** triggers whenever this state is entered **/
+	function onEnter():Void;
 
-    /** triggers whenever this state is entered **/
-    function onEnter():Void;
-    /** triggers whenever this state is destroyed **/
-    function onDestroy():Void;
-    /** triggers whenever this state is not active **/
-    function onSuspend():Void;
-    /** triggers whenever this state is back to active **/
-    function onResume():Void;
-    /** triggers every frame **/
-    function update(tmod:Float):Void;
+	/** triggers whenever this state is destroyed **/
+	function onDestroy():Void;
+
+	/** triggers whenever this state is not active **/
+	function onSuspend():Void;
+
+	/** triggers whenever this state is back to active **/
+	function onResume():Void;
 }
--- src/pirhana/utils/state/StateManager.hx
+++ src/pirhana/utils/state/StateManager.hx
@@ -1,66 +1,71 @@
 package pirhana.utils.state;
 
-class StateManager<T:IState>{
+class StateManager<T:IState> {
+	private var states:Array<T>;
 
-    private var states:Array<T>;
+	/** the currently active state. **/
+	public var current(get, null):T;
 
-    /** the currently active state. **/
-    public var current (get, null):T;
-    /** the previously active state. **/
-    public var previous (get, null):T;
-    /** a copy of the states array. **/
-    public var stack (get, null):Array<T>;
+	/** the previously active state. **/
+	public var previous(get, null):T;
 
-    /** creates a new state manager. It is both a state stack and a state machine. **/
-    public function new(){
-        this.states = new Array();
-    }
+	/** a copy of the states array. **/
+	public var stack(get, null):Array<T>;
 
-    /** removes all states and set the given one. **/
-    public function set(state:T){
-        while (states.length > 0){
-            current.onDestroy();
-            states.pop();
-        }
-        states.push(state);
-        current.onEnter();
-    }
+	/** creates a new state manager. It is both a state stack and a state machine. **/
+	public function new() {
+		this.states = new Array();
+	}
 
-    /** replaces the current state with the given one. **/
-    public function replace(state:T){
-        current.onDestroy();
-        states.pop();
-        states.push(state);
-        current.onEnter();
-    }
+	/** removes all states and set the given one. **/
+	public function set(state:T) {
+		while (states.length > 0) {
+			current.onDestroy();
+			states.pop();
+		}
+		states.push(state);
+		current.onEnter();
+	}
 
-    /** pushes the given state on top of the stack. **/
-    public function push(state:T){
-        current.onSuspend();
-        states.push(state);
-        current.onEnter();
-    }
+	/** replaces the current state with the given one. **/
+	public function replace(state:T) {
+		if (current != null) {
+			current.onDestroy();
+			states.pop();
+		}
+		states.push(state);
+		current.onEnter();
+	}
 
-    /** pops the current state and resume the previous one. **/
-    public function pop(){
-        if (current != null){
-            current.onDestroy();
-        }
-        states.pop();
-        if (current != null){
-            current.onResume();
-        }
-    }
+	/** pushes the given state on top of the stack. **/
+	public function push(state:T) {
+		if (current != null) {
+			current.onSuspend();
+		}
+		states.push(state);
+		current.onEnter();
+	}
 
-    inline function get_current(){
-        return states[states.length-1];
-    }
+	/** pops the current state and resume the previous one. **/
+	public function pop() {
+		if (current != null) {
+			current.onDestroy();
+		}
+		states.pop();
+		if (current != null) {
+			current.onResume();
+		}
+	}
 
-    inline function get_previous(){
-        return states[states.length-2];
-    }
+	inline function get_current() {
+		return states[states.length - 1];
+	}
 
-    inline function get_stack(){
-        return states.copy();
-    }
+	inline function get_previous() {
+		return states[states.length - 2];
+	}
+
+	inline function get_stack() {
+		return states.copy();
+	}
 }
