e0cf3a337834f272a5f0547908af57cb05497446
Christophe Bessette <christophe.bessette@hotmail.com>
2024-09-26 14:31:54 -0400

    added delay to glint shader

--- src/pirhana/heaps/shaders/Glint.hx
+++ src/pirhana/heaps/shaders/Glint.hx
@@ -1,28 +1,52 @@
 package pirhana.heaps.shaders;
 
-class Glint extends hxsl.Shader
-{
-	static var SRC =
-		{
-			@:import h3d.shader.Base2d;
-			@param var speed:Float = 0.6;
-			@param var linewidth:Float = 0.8;
-			@param var grad:Float = 3.0;
-			@param var intensity:Float = 0.6;
-			function fragment()
-			{
-				var col1 = vec4(0.3, 0.0, 0.0, 1.0);
-				var col2 = vec4(0.85, 0.85, 0.85, 1.0);
-				var color = textureColor;
-				var linepos = input.uv;
+class Glint extends hxsl.Shader {
+	static var SRC = {
+		@:import h3d.shader.Base2d;
 
-				linepos.x = linepos.x - mod(time * speed, 2.0) + 0.5;
+		/**
+			Glint line speed.
+		**/
+		@param var speed:Float = 0.8;
 
-				var y = linepos.x * grad;
-				var s = smoothstep(y - linewidth, y, linepos.y) - smoothstep(y, y + linewidth, linepos.y);
+		/**
+			Glint line width.
+		**/
+		@param var linewidth:Float = 0.8;
 
-				pixelColor = color + ((s * col1) + (s * col2)) * intensity;
-				pixelColor.a = color.a;
-			}
+		/**
+			Gradiant angle.
+		**/
+		@param var grad:Float = 3.0;
+
+		/**
+			Glint strength.
+		**/
+		@param var intensity:Float = 0.5;
+
+		/**
+			Delay between each passing glint.
+		**/
+		@param var delay:Float = 2.0;
+
+		/**
+			Fragment shader.
+		**/
+		function fragment() {
+			var col1 = vec4(0.3, 0.0, 0.0, 1.0);
+			var col2 = vec4(0.85, 0.85, 0.85, 1.0);
+			var color = textureColor;
+			var linepos = input.uv;
+
+			var tdelay = 1.0 + delay;
+
+			linepos.x = linepos.x - clamp(mod(time * speed * (1.0 / tdelay), 2.0), 0.0, 1.0) * tdelay + 0.5;
+
+			var y = linepos.x * grad;
+			var s = smoothstep(y - linewidth, y, linepos.y) - smoothstep(y, y + linewidth, linepos.y);
+
+			pixelColor = color + ((s * col1) + (s * col2)) * intensity;
+			pixelColor.a = color.a;
 		}
+	}
 }
